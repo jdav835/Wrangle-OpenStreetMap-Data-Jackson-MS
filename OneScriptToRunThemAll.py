0eafe86dde7f45c1039c34a202774eae31999073
Jennifer Davis <jdav835@wgu.edu>
2021-08-22 19:37:05 +0000

    Replace OneScriptToRunThemAll.py

--- OneScriptToRunThemAll.py
+++ OneScriptToRunThemAll.py
@@ -1,30 +1,24 @@
 import toCSV
 import audit
 import toSQL
-
+import sqlQueries
 FILE = "jackson_ms.osm"
 # Master Script that runs all cleaning and auditing scripts
 
 # counts keys
-#audit.count_keys(FILE)
+audit.count_keys(FILE)
 
 # counts unique users
-#audit.count_users(FILE)
+audit.count_users(FILE)
 
 # counts tags
-#audit.count_tags(FILE)
+audit.count_tags(FILE)
 
 # processes data and converts to CSVs
-toCSV.process_map(FILE, validate=False)
+toCSV.process_map(FILE, validate=True)
 
 # Creates database and Inserts CSVs
-#toSQL.toSQL()
+toSQL.toSQL()
 
 
-
-
-#toSQL.query_data()
-
-audit.find_phone(FILE)
-#print(audit.cleanphone("601-256-8659"))
-#print(audit.cleanphone("+1 601-807-4615"))
+sqlQueries.query_data()
